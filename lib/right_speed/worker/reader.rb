859a8738f4bbee8d4f5245fba2de50eebc666377
Satoshi Moris Tagomori <tagomoris@gmail.com>
2021-09-09 18:02:46 +0900

    remove outdated comments

--- lib/right_speed/worker/reader.rb
+++ lib/right_speed/worker/reader.rb
@@ -7,14 +7,9 @@ module RightSpeed
       def run
         @ractor = Ractor.new(@id, @handler) do |id, handler|
           logger = RightSpeed.logger
-          # read and parse requests from sockets in Async manner (Fiber#scheduler ?)
-          # https://github.com/fluent/fluentd/blob/master/lib/fluent/plugin/in_http.rb
           while conn = Ractor.receive
             begin
               handler.session(conn).process
-              # data = conn.read
-              # logger.info "[read|#{id}] Content: #{data}"
-              # conn.write "200 OK"
             rescue => e
               logger.error { "Unexpected error: #{e.message}\n" + e.backtrace.map{"\t#{_1}\n"}.join }
               # TODO: print backtrace in better way
